e6502feb80c2c54701f1107fb52193ee1316cc9d
Patrick Lai <2010.PLai@gmail.com>
2020-10-05 21:04:46 -0700

    Modal component.

--- src/Modal.vue
+++ src/Modal.vue
@@ -1,46 +1,35 @@
 <!--
- * Vue.js templates for admin pages.
+ * Modal dialog component.
  * Author: Patrick Lai
  *
  * @todo Localization.
  * @copyright Copyright (c) 2020 Patrick Lai
 -->
-<script type="text/x-template" id="pl2010-modal-template">
-	<transition name="pl2010-modal">
-		<div class="pl2010-modal-mask">
-			<div class="pl2010-modal-wrapper">
-				<div class="pl2010-modal-container">
-					<div class="pl2010-modal-header">
-						<div slot="title" class="title">
-							<slot name="title">Default Title</slot>
-						</div>
-						<div class="ctrl-btns">
-							<slot name="ctrl-btns"></slot>
-							<button type="button" @click="$emit('close')">x</button>
-						</div>
-						<div>&nbsp;</div>
-					</div>
-					<div class="pl2010-modal-body">
-						<slot name="body">default body</slot>
-					</div>
-					<div class="pl2010-modal-footer">
-						<slot name="footer">
-							default footer
-							<button type="button" @click="$emit('close')">OK</button>
-						</slot>
-					</div>
+<template>
+<div class="pl2010-modal-mask">
+	<div class="pl2010-modal-wrapper">
+		<div class="pl2010-modal-container">
+			<div class="pl2010-modal-header">
+				<div class="title">
+					<h3><slot name="title">Default Title</slot></h3>
 				</div>
+				<div class="ctrl-btns">
+					<slot name="ctrl-btns"></slot>
+					<button type="button" @click="$emit('close')">&cross;</button>
+				</div>
+				<div>&nbsp;</div>
+			</div>
+			<div class="pl2010-modal-body">
+				<slot name="body">default body</slot>
+			</div>
+			<div class="pl2010-modal-footer">
+				<slot name="footer">
+					<button type="button" @click="$emit('close')">OK</button>
+				</slot>
 			</div>
 		</div>
-	</transition>
-</script>
-
-<script type="text/javascript">
-	jQuery(document).ready(function() {
-		Vue.component('pl2010-modal', {
-			template: '#pl2010-modal-template'
-		});
-	});
-</script>
+	</div>
+</div>
+</template>
 
 <!-- vim: set ts=2 noexpandtab fdm=marker syntax=html: ('zR' to unfold all) -->
